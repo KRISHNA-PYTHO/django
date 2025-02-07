7cd2984a4e0d46d61ad6f35674db632c016829a4
Krishna mourya <maurya1389@gmail.com>
2025-02-07 19:20:50 +0530

    upate

--- template_inheritance/core/urls.py
+++ template_inheritance/core/urls.py
@@ -3,8 +3,8 @@ from .  import views
 
 urlpatterns = [
     path('',views.index),
-    path('',views.about),
-    path('',views.contact),
-    path('',views.customer),
-    path('',views.project)
+    path('about/',views.about),
+    path('contact/',views.contact),
+    path('customer/',views.customer),
+    path('project/',views.project)
     ]
