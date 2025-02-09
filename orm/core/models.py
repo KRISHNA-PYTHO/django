6d35741239e47ea60bc4770a3f1b9b1d8f84c778
Krishna mourya <maurya1389@gmail.com>
2025-02-09 13:02:21 +0530

    one file added

--- orm/core/models.py
+++ orm/core/models.py
@@ -6,5 +6,4 @@ class Marvel(models.Model):
     last_name = models.CharField(max_length=50)
     heroic_name = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.heroic_name
+   
